929512c469511758c9e444f11a416def03adf48b
Hunter Baker <hunterbaker@me.com>
2024-03-05 21:05:59 -0500

    Add basic implementation of a service to set the effect

--- dexi_led_strip/dexi_led_strip/led_strip_node.py
+++ dexi_led_strip/dexi_led_strip/led_strip_node.py
@@ -1,13 +1,22 @@
+import time
+from typing import Any
 from threading import Thread, Event
 
 import rclpy
 from rclpy.node import Node
+
+from adafruit_led_animation.animation.solid import Solid
+from adafruit_led_animation.animation.rainbow import Rainbow
 
 from dexi_msgs.srv import SetLedEffect
 
 from util import SPI, run_anim_until_done
 from neopixel_ring_spi import NeoPixelRing_SPI
 from channel_wrap_animation import ChannelWrapAnim
+
+ANIMATIONS = {
+    
+}
 
 
 class LEDStripNode(Node):
@@ -28,6 +37,8 @@ class LEDStripNode(Node):
 
         channel_count = len(self.pixel_order)
 
+        self.set_service = self.create_service(SetLedEffect, 'set', self.set_callback)
+
         self.pixels = NeoPixelRing_SPI(
             spi,
             led_count,
@@ -39,31 +50,66 @@ class LEDStripNode(Node):
 
         self.boot_anim = ChannelWrapAnim(self.pixels, 0.05)
 
-        self.current_animation = None
+        self.base_color = [0] * channel_count
+
+        self._current_animation = None
+        self.animation_duration = float('nan')
+        self._animation_start_time = -1
         self.animation_changed = Event()
 
         self.update_thread = Thread(target=self.update_loop, daemon=True)
         self.play_boot_anim()
         self.update_thread.start()
 
-    def play_boot_anim() -> None:
+    @property
+    def current_animation(self) -> Any:
+        return self._current_animation
+
+    @current_animation.setter
+    def current_animation(self, new_animation: Any) -> None:
+        self._current_animation = new_animation
+        self._animation_start_time = time.time()
+        self.animation_changed.set()
+
+    def play_boot_anim(self) -> None:
         if not self.update_thread.is_alive():
             self.pixels.fill(0)
             run_anim_until_done(self.boot_anim)
 
-    def update_loop() -> None:
-        animation = None
-        self.animation_changed.set()
+    def set_callback(self, request: SetLedEffect.Request, response: SetLedEffect.Response) -> SetLedEffect.Response:
+        # ToDo: add speed and argument to msg
+        color = request.r, request.g, request.b
+        if request.effect == 'base':
+            self.current_animation = None
+        elif request.effect in ('', 'solid'):
+            self.current_animation = Solid(self.pixels, color)
+        elif request.effect == 'rainbow':
+            self.animation_duration = request.duration
+            self.current_animation = Rainbow(self.pixels, 0.05)
+        else:
+            response.success = False
+            response.message = f'Unknown effect: \"{request.effect}\"'
+            return response
+        response.success = True
+        response.message = 'Success'
+
+    def update_loop(self) -> None:
+        animation = self.current_animation
         while True:
             if self.animation_changed.is_set():
                 self.animation_changed.clear()
                 animation = self.current_animation
 
-            if animation is None:
-                self.animation_changed.wait()
-            else:
+            if animation is not None:
                 animation.animate()
-        
+            else:
+                self.pixels.fill(self.base_color)
+            
+            if isinstance(animation, Solid) or animation is None:
+                self.animation_changed.wait(self.animation_duration)
+            elif (time.time() - self._animation_start_time) >= self.animation_duration:
+                self.current_animation = None
+
 
 def main(args=None):
     rclpy.init(args=args)
