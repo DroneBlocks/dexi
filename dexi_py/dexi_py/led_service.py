468eac0a742140fcc3fb707ef129e83fbd45bbbd
Dennis Baldwin <dennisbaldwin@gmail.com>
2024-08-13 21:03:02 -0500

    LED improvements WIP

--- dexi_py/dexi_py/led_service.py
+++ dexi_py/dexi_py/led_service.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from led_msgs.srv import SetLED
+from led_msgs.srv import SetLEDPixel
+from led_msgs.srv import SetLEDColor
 import board
 import neopixel
 
@@ -8,22 +9,25 @@ class LEDService(Node):
 
     def __init__(self):
         super().__init__('led_service')
-        self.srv = self.create_service(SetLED, 'set_led', self.set_led_callback)
-        self.pixel_pin = board.D10
-        self.num_pixels = 30
+        self.led_service = self.create_service(SetLEDPixel, 'set_led_pixel', self.set_led_pixel_callback)
+        self.led_color_service = self.create_service(SetLEDColor, 'set_led_color', self.set_led_color_callback)
+        self.pixel_pin = board.D12
+        self.num_pixels = 10
         self.pixel_order = neopixel.GRB
         self.pixels = neopixel.NeoPixel(self.pixel_pin, self.num_pixels, brightness=0.2, auto_write=False, pixel_order=self.pixel_order)
 
 
-    def set_led_callback(self, request, response):
+    def set_led_pixel_callback(self, request, response):
         self.get_logger().info(str(request))
         self.pixels[request.index] = (request.r, request.g, request.b)
         self.pixels.show()
         response.success = True
         return response
 
-    def set_leds_callback(self):
-        return
+    def set_led_color_callback(self, request, response):
+        self.get_logger().info(str(request))
+        response.success = True
+        return response
 
     def rainbow(self):
         return
