83c0599c07a7cf3c9cebd451843932a0f8f829d0
Dennis Baldwin <dennisbaldwin@gmail.com>
2024-11-10 13:47:59 -0600

    Simplify node launching

--- dexi/scripts/install.bash
+++ dexi/scripts/install.bash
@@ -3,8 +3,10 @@
 echo Setting up dexi.service...
 
 sudo cp "$(dirname "$0")/dexi.service" /etc/systemd/system/
+sudo cp "$(dirname "$0")/dexi_avr.service" /etc/systemd/system/
 
 sudo systemctl daemon-reload
-
 sudo systemctl enable dexi.service
 sudo systemctl restart dexi.service
+
+sudo systemctl enable dexi_avr.service
