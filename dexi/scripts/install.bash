41dbb512e698e6fa2fb0992b36004a2213138fc7
Dennis Baldwin <dennisbaldwin@gmail.com>
2024-11-11 16:26:26 -0600

    Enable AVR service

--- dexi/scripts/install.bash
+++ dexi/scripts/install.bash
@@ -6,8 +6,8 @@ sudo cp "$(dirname "$0")/dexi.service" /etc/systemd/system/
 sudo cp "$(dirname "$0")/dexi_avr.service" /etc/systemd/system/
 
 sudo systemctl daemon-reload
-sudo systemctl enable dexi.service
-sudo systemctl restart dexi.service
+# sudo systemctl enable dexi.service
+# sudo systemctl restart dexi.service
 
-#sudo systemctl disable dexi.service
-#sudo systemctl enable dexi_avr.service
+sudo systemctl enable dexi_avr.service
+sudo systemctl restart dexi_avr.service
