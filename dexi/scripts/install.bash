66a4b47568203debf5644b1db14425199cea8e65
Dennis Baldwin <dennisbaldwin@gmail.com>
2025-03-15 12:40:48 -0500

    Disable pigpiod for now

--- dexi/scripts/install.bash
+++ dexi/scripts/install.bash
@@ -9,8 +9,8 @@ sudo cp "$(dirname "$0")/pigpiod.service" /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable dexi.service
 sudo systemctl restart dexi.service
-sudo systemctl enable pigpiod.service
-sudo systemctl restart pigpiod.service
+#sudo systemctl enable pigpiod.service
+#sudo systemctl restart pigpiod.service
 
 #sudo systemctl disable dexi.service
 #sudo systemctl enable dexi_avr.service
